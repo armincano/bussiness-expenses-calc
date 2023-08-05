51d4038489232275012321eb709b2bc7abe5e58a
anggifit <abonaldy@gmail.com>
2023-08-05 12:37:30 +0200

    Detalles del item seleccionado

--- my-app/src/ExpenseItem/ExpenseItem.js
+++ my-app/src/ExpenseItem/ExpenseItem.js
@@ -1,6 +1,18 @@
+import React from "react";
+import "./ExpenseItem.css"
+
+
 function ExpenseItem(props) {
     return(
-        <div>Expense Item</div>
+        <div className="expense_item_container">
+            <div className="expense_item_details">
+                <h2>Expense Details</h2>
+                <p>Description: {props.description}</p>
+                <p>Category: {props.category}</p>
+                <p>Amount: {props.amount}</p>
+                <p>Date: {props.date}</p>
+            </div>
+        </div>
     )
 }
 
